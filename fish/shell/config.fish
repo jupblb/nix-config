93c7361963caadf50c4d92d5a8c3c242325dc4ac
jupblb <mpkielbowicz@gmail.com>
2019-11-25 22:18:44 +0100

    Switch to regular Gruvbox (light)

--- fish/shell/config.fish
+++ fish/shell/config.fish
@@ -18,3 +18,10 @@ set -g theme_display_date           no
 # General env variables
 set -x SBT_OPTS           "-Xms2G -Xmx8G -Xss4m"
 set -x COURSIER_CACHE     "/home/jupblb/.cache/coursier"
+
+set -l GRUVBOX_SCRIPT ~/.config/fish/gruvbox.sh
+if test -f $GRUVBOX_SCRIPT
+  bash $GRUVBOX_SCRIPT
+end
+
+theme_gruvbox light hard
--- nixos/overlays/vim/default.nix
+++ nixos/overlays/vim/default.nix
@@ -1,19 +1,9 @@
 { buildVimPluginFrom2Nix, fetchFromGitHub, vim_configurable, vimPlugins }:
 
-let
-  gruvbox-material = buildVimPluginFrom2Nix {
-    name = "gruvbox-material";
-    src  = fetchFromGitHub {
-      owner  = "sainnhe";
-      repo   = "gruvbox-material";
-      rev    = "0a0e6b00f78d7808989143fad77b19a6f3676e0e";
-      sha256 = "1a7kvx9jb7nkbbs7rv0x10lzfq1h4sk5l0dljnnps6f31ynnqwxm";
-    };
-  };
-in vim_configurable.customize {
+vim_configurable.customize {
   name                              = "vim";
   vimrcConfig.customRC              = builtins.readFile(./vimrc);
   vimrcConfig.packages.myVimPackage = with vimPlugins; {
-    start = [ airline ctrlp easymotion gitgutter gruvbox-material ];
+    start = [ airline ctrlp easymotion gitgutter gruvbox-community ];
   };
 }
