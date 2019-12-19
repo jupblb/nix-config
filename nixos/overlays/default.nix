10ce684fcb5137ce09ef9219e53b4e9d7992faab
jupblb <mpkielbowicz@gmail.com>
2019-12-19 14:52:05 +0100

    Bump IntelliJ version

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -8,10 +8,10 @@ in {
   gnome-screenshot' = pkgs.gnome3.gnome-screenshot;
   idea-ultimate'    = pkgs.jetbrains.idea-ultimate.overrideAttrs (old: rec {
     name    = "idea-ultimate-${version}";
-    version = "2019.3";
+    version = "2019.3.1";
     src     = pkgs.buildPackages.fetchurl {
       url    = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-      sha256 = "0jxkx8xq1a73mrvzjaniapiznwr77hli9srwgkxa6zsq2kmix6vk";
+      sha256 = "0cjmcpsfnrhs2ggv4pa0pyck08xvclwazqp1i1ygdii4qlvkam47";
     };
   });
   sbt'              = pkgs.sbt.override { jre = pkgs.graalvm8; };
