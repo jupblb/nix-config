22ecaad579be94b8fa2a73ad359ad51d1fc2f768
jupblb <mpkielbowicz@gmail.com>
2020-01-07 16:33:06 +0100

    Revert to use nixpkgs version of IntelliJ

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -6,14 +6,7 @@ in {
   all-hies'         = all-hies.selection { selector = p: p; };
   diff-so-fancy'    = pkgs.gitAndTools.diff-so-fancy;
   gnome-screenshot' = pkgs.gnome3.gnome-screenshot;
-  idea-ultimate'    = pkgs.jetbrains.idea-ultimate.overrideAttrs (old: rec {
-    name    = "idea-ultimate-${version}";
-    version = "2019.3.1";
-    src     = pkgs.buildPackages.fetchurl {
-      url    = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-      sha256 = "0cjmcpsfnrhs2ggv4pa0pyck08xvclwazqp1i1ygdii4qlvkam47";
-    };
-  });
+  idea-ultimate'    = pkgs.jetbrains.idea-ultimate.override { jdk = pkgs.jetbrains.jdk; };
   sbt'              = pkgs.sbt;#.override { jre = pkgs.graalvm8; };
   vaapiIntel'       = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   vim'              = pkgs.callPackage ./vim { inherit (pkgs.vimUtils) buildVimPluginFrom2Nix; }; 
