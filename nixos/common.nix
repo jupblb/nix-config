c394c7269716565c27993b2d4fc4b09398214b46
jupblb <mpkielbowicz@gmail.com>
2020-02-01 20:30:07 +0100

    Blacklist ads in hosts file

--- nixos/common.nix
+++ nixos/common.nix
@@ -1,5 +1,11 @@
 { config, lib, pkgs, ... }:
 
+let
+  hosts = pkgs.fetchurl {
+    url = "http://sbc.io/hosts/alternates/fakenews-gambling/hosts";
+    sha256 = "sha256:1jjfhdnql44zd47wvvxgw3jwysx5ijmbdy5dnhh7694pq0rl3r9s";
+  };
+in
 {
   boot = {
     kernel.sysctl."fs.inotify.max_user_watches" = 524288;
@@ -71,6 +77,7 @@
   i18n.defaultLocale    = "en_US.UTF-8";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "pl_PL.UTF-8/UTF-8" ];
 
+  networking.extraHosts               = builtins.readFile hosts;
   networking.firewall.allowedTCPPorts = [ 22 ];
   networking.nameservers              = [ "1.1.1.1" "8.8.8.8" ];
   networking.networkmanager.enable    = true;
