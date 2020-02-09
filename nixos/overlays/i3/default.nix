d35765598ab5c8ac0d98196c8f44f7d2efecfd6f
jupblb <mpkielbowicz@gmail.com>
2020-02-09 17:58:10 +0100

    Reformat i3 overlay

--- nixos/overlays/i3/default.nix
+++ nixos/overlays/i3/default.nix
@@ -1,8 +1,8 @@
 { i3-gaps, symlinkJoin, makeWrapper }:
 
 symlinkJoin {
-  name = "i3";
+  name        = "i3";
   buildInputs = [ makeWrapper ];
-  paths = [ i3-gaps ];
-  postBuild = ''wrapProgram "$out/bin/i3" --add-flags "-c /etc/i3/config"'';
+  paths       = [ i3-gaps ];
+  postBuild   = ''wrapProgram "$out/bin/i3" --add-flags "-c /etc/i3/config"'';
 }
