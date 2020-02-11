75eafe7b063248d08291738c11807d07fe903400
jupblb <mpkielbowicz@gmail.com>
2020-02-11 16:25:51 +0100

    Update vim config

--- nixos/overlays/vim/default.nix
+++ nixos/overlays/vim/default.nix
@@ -2,7 +2,12 @@
 
 vim_configurable.customize {
   name                              = "vim";
-  vimrcConfig.customRC              = builtins.readFile(./vimrc);
+  vimrcConfig.customRC              = ''
+    ${builtins.readFile(./vimrc)}
+    " Set custom location for netrw and viminfo
+    let g:netrw_home='~/.cache/vim_netrwhist'
+    set viminfo+='1000,n~/.cache/viminfo
+  '';
   vimrcConfig.packages.myVimPackage = with vimPlugins; {
     start = [ airline ctrlp gitgutter gruvbox-community ];
   };
