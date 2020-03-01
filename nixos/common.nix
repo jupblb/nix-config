fb80b6240a573e4fdbbaa4c5a389fb236114efd7
jupblb <mpkielbowicz@gmail.com>
2020-03-01 19:32:52 +0100

    Show untracked files in fish

--- nixos/common.nix
+++ nixos/common.nix
@@ -103,6 +103,7 @@
     fish.enable                  = true;
     fish.interactiveShellInit    = ''
       set -g __fish_git_prompt_showdirtystate "yes"
+      set -g __fish_git_prompt_showuntrackedfiles "yes"
       ${pkgs.xdg-user-dirs}/bin/xdg-user-dirs-update
       function fish_greeting; ${pkgs.fortune}/bin/fortune -sa; end
     '';
