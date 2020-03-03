1a22b44908e5040089ec4b2151b9eb45aad21bd7
jupblb <mpkielbowicz@gmail.com>
2020-03-03 11:24:29 +0100

    Replace dmenu with bemenu

--- nixos/overlays/i3/default.nix
+++ nixos/overlays/i3/default.nix
@@ -1,5 +1,5 @@
 {
-  dmenu, dunst, firefox, franz, gnome-screenshot,
+  bemenu, dunst, firefox, franz, gnome-screenshot,
   i3-gaps, i3status', idea-ultimate', imv, lib,
   makeWrapper, mpv, pavucontrol, qutebrowser, redshift,
   symlinkJoin, writeTextFile, zoom-us
@@ -12,8 +12,9 @@ let
       exec --no-startup-id ${dunst}/bin/dunst -conf ${./dunstrc}
       exec --no-startup-id ${redshift}/bin/redshift -l 51.12:17.05
       set $browser env QT_SCALE_FACTOR=2 ${qutebrowser}/bin/qutebrowser --basedir ~/.local/share/qutebrowserx
-      set $menu ${dmenu}/bin/dmenu_path | ${dmenu}/bin/dmenu_run \
-        -fn 'PragmataPro Mono Liga:bold:pixelsize=40' -nb '#282828' -nf '#f9f5d7' -sb '#f9f5d7' -sf '#282828'
+      set $menu ${bemenu}/bin/bemenu-run \
+        --fn 'PragmataPro 26' -p "" --fb '#282828' --ff '#f9f5d7' --hb '#98971a' --hf '#f9f5d7' --nb '#282828' --nf '#f9f5d7' \
+        --sf '#282828' --sb '#f9f5d7' --tf '#f9f5d7' --tb '#282828' | xargs swaymsg exec --
       set $print ${gnome-screenshot}/bin/gnome-screenshot -i --display=:0
       ${builtins.readFile(../common-wm-config)}
       ${builtins.readFile(./i3-config)}
