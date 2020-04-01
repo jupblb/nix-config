3b910c63689273bfbd009b6a87a61f7d08aaacb4
jupblb <mpkielbowicz@gmail.com>
2020-04-01 18:23:38 +0200

    Fix firefox on wayland

--- firefox/user.js
+++ firefox/user.js
@@ -3,9 +3,6 @@ user_pref("extensions.pocket.enabled", false);
 user_pref("general.smoothScroll", false);
 user_pref("mousewheel.min_line_scroll_amount", 64);
 
-user_pref("layers.acceleration.force-enabled", true);
-user_pref("gfx.webrender.all", true);
-
 user_pref("full-screen-api.warning.timeout", 0);
 
 user_pref("network.protocol-handler.expose.magnet", false);
