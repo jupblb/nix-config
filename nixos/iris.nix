fdeb8eba8104ec5c76bbf5e1306a2f6a8bab223b
jupblb <mpkielbowicz@gmail.com>
2020-04-11 23:18:41 +0200

    Iris eth device name fix

--- nixos/iris.nix
+++ nixos/iris.nix
@@ -67,7 +67,7 @@ in
   networking.firewall.allowedTCPPorts       = [ 53 67 80 443 ];
   networking.firewall.allowedUDPPorts       = [ 53 67 80 443 ];
   networking.hostName                       = "iris";
-  networking.interfaces.eno2.ipv4.addresses = [ { address = "192.168.1.7"; prefixLength = 24; } ];
+  networking.interfaces.eth0.ipv4.addresses = [ { address = "192.168.1.7"; prefixLength = 24; } ];
   networking.wireless.enable                = false;
 
   nix.maxJobs = 4;
