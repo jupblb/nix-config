8836e5bda98758fc2322db60b860524ef6ec0842
jupblb <mpkielbowicz@gmail.com>
2020-05-09 13:47:08 +0200

    Use extraMakeWrapperArgs in neovim overlay

--- overlays/neovim/default.nix
+++ overlays/neovim/default.nix
@@ -1,44 +1,9 @@
 {
-  bash-language-server, eslint, fetchFromGitHub, lib, makeWrapper, neovim,
-  nodejs_latest, npm, openjdk11, python-language-server, ranger', ripgrep,
-  symlinkJoin, vimPlugins, vimUtils, writeScriptBin
+  bash-language-server, eslint, fetchFromGitHub, lib, neovim, nodejs_latest,
+  npm, openjdk11, python-language-server, ranger', ripgrep, vimPlugins, vimUtils
 }:
 
 let
-  neovim'     = neovim.override {
-    configure   = {
-      customRC = builtins.readFile(./init.vim);
-
-      plug.plugins = with vimPlugins; [
-        coc-nvim
-        denite
-
-        airline
-        bclose-vim
-        coc-eslint
-        coc-java
-        coc-json
-        coc-metals
-        coc-python
-#       coc-rust-analyzer
-        coc-tsserver
-        easymotion
-        fugitive
-        goyo
-        gruvbox-community
-        vim-devicons
-        vim-nix
-        vim-ranger'
-        vim-signify
-        vim-startify
-      ];
-    };
-
-    withNodeJs  = true;
-    withPython  = false;
-    withPython3 = true;
-    withRuby    = false;
-  };
   vim-ranger' = vimUtils.buildVimPluginFrom2Nix {
     pname   = "ranger-vim";
     version = "2020-05-08";
@@ -49,22 +14,51 @@ let
       sha256 = "0i2d88yyfjv4gn3zn7jzv5pf94vzllvxmnmi3hdjddrhl2xppsza";
     };
   };
-in symlinkJoin {
-  buildInputs = [ makeWrapper ];
-  name        = "nvim";
-  paths       = [ neovim' ];
-  postBuild   = ''
-    wrapProgram "$out/bin/nvim" \
-      --prefix PATH : ${lib.makeBinPath[
-        bash-language-server
-        eslint
-        nodejs_latest npm
-        openjdk11
-        python-language-server
-        ranger' ripgrep
-      ]} \
-      --set JAVA_HOME ${openjdk11}/lib/openjdk
+in neovim.override {
+  configure   = {
+    customRC = builtins.readFile(./init.vim);
 
-    ln -sfn $out/bin/nvim $out/bin/vim
+    plug.plugins = with vimPlugins; [
+      coc-nvim
+      denite
+
+      airline
+      bclose-vim
+      coc-eslint
+      coc-java
+      coc-json
+      coc-metals
+      coc-python
+#     coc-rust-analyzer
+      coc-tsserver
+      easymotion
+      fugitive
+      goyo
+      gruvbox-community
+      vim-devicons
+      vim-nix
+      vim-ranger'
+      vim-signify
+      vim-startify
+    ];
+  };
+
+  extraMakeWrapperArgs = ''
+    --prefix PATH : ${lib.makeBinPath[
+      bash-language-server
+      eslint
+      nodejs_latest npm
+      openjdk11
+      python-language-server
+      ranger' ripgrep
+    ]} \
+    --set JAVA_HOME ${openjdk11}/lib/openjdk
   '';
+
+  vimAlias = true;
+
+  withNodeJs  = true;
+  withPython  = false;
+  withPython3 = true;
+  withRuby    = false;
 }
