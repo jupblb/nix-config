5a1a1430439d324a3fe49ac81e3a061cd41ee96e
jupblb <mpkielbowicz@gmail.com>
2020-07-13 15:33:46 +0200

    Switch to terminal version of emacs

--- overlays/emacs/default.nix
+++ overlays/emacs/default.nix
@@ -1,29 +1,13 @@
 {
-  autoflake, autoreconfHook, bash-language-server, clang-tools, emacs,
-  fetchFromGitHub, flake8, lib, makeWrapper, metals, pandoc, pytest,
-  python-language-server, python3, ripgrep, texinfo, symlinkJoin, wayland,
-  wayland-protocols
+  autoflake, bash-language-server, clang-tools, emacs-nox, flake8, lib,
+  makeWrapper, metals, pandoc, pytest, python-language-server, python3, ripgrep,
+  symlinkJoin
 }:
 
-let emacs28 = emacs.overrideAttrs(old: {
-  buildInputs       = old.buildInputs ++ [ wayland wayland-protocols ];
-  configureFlags    = old.configureFlags ++ [
-    "--without-x" "--with-cairo" "--with-modules"
-  ];
-  nativeBuildInputs = old.nativeBuildInputs ++ [ autoreconfHook texinfo ];
-  patches           = [];
-
-  src = fetchFromGitHub {
-    owner  = "masm11";
-    repo   = "emacs";
-    rev    = "pgtk";
-    sha256 = "0wbjf88hyl6b1ikqh1rfgaib5y0v6py6k6s1bgi1sqq4zf5afgsv";
-  };
-});
 in symlinkJoin {
   buildInputs = [ makeWrapper ];
   name        = "emacs";
-  paths       = emacs28;
+  paths       = emacs-nox;
   postBuild   = ''
     wrapProgram $out/bin/emacs \
       --prefix PATH : ${lib.makeBinPath[
