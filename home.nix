7820d81f3a5e01553b10475e3084006c414d7478
jupblb <mpkielbowicz@gmail.com>
2020-10-22 20:35:48 +0200

    Add calendar.vim

--- home.nix
+++ home.nix
@@ -96,6 +96,20 @@
             let g:lightline = { 'colorscheme': 'gruvbox' }
           '';
         } {
+          plugin = calendar-vim;
+          config = ''
+            let g:calendar_google_calendar = 1
+            let g:calendar_google_task = 1
+            nnoremap <Leader>c :Calendar<CR>
+            autocmd FileType calendar nmap <silent> <buffer> <CR> :<C-u>call
+              \ vimwiki#diary#calendar_action(b:calendar.day().get_day(),
+              \ b:calendar.day().get_month(), b:calendar.day().get_year(),
+              \ b:calendar.day().week(), "V")<CR>
+            if filereadable(expand("~/.config/nvim/google.vim"))
+              source ~/.config/nvim/google.vim
+            endif
+          '';
+        } {
           plugin = completion-nvim;
           config = ''
             packadd completion-nvim
