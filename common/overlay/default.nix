954e41c6e5b05205232c2cbb545003dce7ac4fc0
jupblb <mpkielbowicz@gmail.com>
2020-12-24 13:43:34 +0100

    Drop delta overlay

--- common/overlay/default.nix
+++ common/overlay/default.nix
@@ -1,10 +1,5 @@
 self: super: {
   fishPlugins    = import ./fish { inherit (super) callPackage; };
-  gitAndTools    = super.gitAndTools // {
-    delta = super.callPackage ./delta {
-      inherit (super.darwin.apple_sdk.frameworks) Security;
-    };
-  };
   neovim-nightly = super.callPackage ./neovim {};
   pragmata-pro   = super.callPackage ./pragmata-pro {};
 }
