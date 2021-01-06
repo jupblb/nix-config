8cd87ec6945927f8f935f412ed7decee1263b46d
jupblb <mpkielbowicz@gmail.com>
2021-01-06 23:09:24 +0100

    Temporary fix for home-manager

--- common/overlay/fish/default.nix
+++ common/overlay/fish/default.nix
@@ -1,7 +1,8 @@
 { callPackage }:
 
 {
-  gruvbox    = callPackage ./gruvbox.nix {};
-  nix-env    = callPackage ./nix-env.nix {};
-  bobthefish = callPackage ./bobthefish.nix {};
+  foreign-env = callPackage ./foreign-env.nix {};
+  gruvbox     = callPackage ./gruvbox.nix {};
+  nix-env     = callPackage ./nix-env.nix {};
+  bobthefish  = callPackage ./bobthefish.nix {};
 }
---
+++ common/overlay/fish/foreign-env.nix
@@ -0,0 +1,8 @@
+{ fetchFromGitHub }:
+
+fetchFromGitHub {
+  owner  = "oh-my-fish";
+  repo   = "plugin-foreign-env";
+  rev    = "master";
+  sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+}
