ce001bc4ebc738e6268c3a3940f24b11a4b86feb
jupblb <mpkielbowicz@gmail.com>
2021-01-17 11:00:13 +0100

    Fix fish plugin change in upstream

--- common/overlay/default.nix
+++ common/overlay/default.nix
@@ -1,10 +1,11 @@
 self: super: {
-  emacs-wrapped  = super.callPackage ./emacs {};
-  fishPlugins    = import ./fish { inherit (super) callPackage; };
-  neovim-nightly = super.callPackage ./neovim {};
-  pragmata-pro   = super.callPackage ./pragmata-pro {};
-  ranger         = super.callPackage ./ranger { ranger = super.ranger; };
-  vimPlugins     = super.vimPlugins // {
+  emacs-wrapped    = super.callPackage ./emacs {};
+  fish-foreign-env = super.fishPlugins.foreign-env;
+  fishPlugins      = import ./fish { inherit (super) callPackage; };
+  neovim-nightly   = super.callPackage ./neovim {};
+  pragmata-pro     = super.callPackage ./pragmata-pro {};
+  ranger           = super.callPackage ./ranger { ranger = super.ranger; };
+  vimPlugins       = super.vimPlugins // {
     ranger-vim = super.callPackage ./neovim/ranger.nix {};
   };
 }
