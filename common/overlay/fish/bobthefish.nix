9d4311a72f0e09679e6841d4762d7d7f33f9b7aa
jupblb <mpkielbowicz@gmail.com>
2021-03-30 21:58:17 +0200

    Fix bobthefish

--- common/overlay/fish/bobthefish.nix
+++ common/overlay/fish/bobthefish.nix
@@ -1,8 +1,13 @@
 { fetchFromGitHub, symlinkJoin }:
 
-fetchFromGitHub {
+let plugin = fetchFromGitHub {
   owner  = "oh-my-fish";
   repo   = "theme-bobthefish";
   rev    = "master";
   sha256 = "06whihwk7cpyi3bxvvh3qqbd5560rknm88psrajvj7308slf0jfd";
+};
+in symlinkJoin {
+  name      = "theme-bobthefish";
+  paths     = [ plugin ];
+  postBuild = "mv $out/functions $out/conf.d";
 }
