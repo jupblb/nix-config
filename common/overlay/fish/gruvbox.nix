5337d1e186759fbbfd24e9d771fba89d66b7ee57
jupblb <mpkielbowicz@gmail.com>
2021-03-30 23:25:09 +0200

    Switch fish gruvbox source to gruvbox-community

--- common/overlay/fish/gruvbox.nix
+++ common/overlay/fish/gruvbox.nix
@@ -1,9 +1,9 @@
 { fetchFromGitHub }:
 
 fetchFromGitHub {
-  owner  = "Jomik";
+  owner  = "gruvbox-community";
   repo   = "fish-gruvbox";
   rev    = "master";
-  sha256 = "0hkps4ddz99r7m52lwyzidbalrwvi7h2afpawh9yv6a226pjmck7";
+  sha256 = "1divh9j36y9dnwdfn594fxrmdk543n315gdxk4ka6z4g8sp5w41f";
 }
 
