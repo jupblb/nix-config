b3f2f6b3c0ed1d9f936b8f40adb33bd14e532da3
jupblb <jupblb@google.com>
2021-04-05 10:23:17 +0200

    Enable minikube on demeter

--- config/prompt.fish
+++ config/prompt.fish
@@ -1,10 +1,10 @@
 function fish_greeting; end
 
+set -g theme_color_scheme solarized-light
 set -g theme_display_cmd_duration no
 set -g theme_display_date no
 set -g theme_display_k8s_context yes
 set -g theme_display_k8s_namespace yes
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme solarized-light
 
 theme_gruvbox light hard
