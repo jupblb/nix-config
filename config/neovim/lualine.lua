677c1c685e5a905b95f8f0b5a5d8418484e9515a
jupblb <jupblb@google.com>
2021-04-15 10:00:05 +0200

    Clean theme config for lualine

--- config/neovim/lualine.lua
+++ config/neovim/lualine.lua
@@ -1,51 +1,51 @@
 local gruvbox_light = {}
 local colors = {
-  white        = '#fbf1c7',
-  black        = '#3c3836',
-  red          = '#9d0006',
-  green        = '#79740e',
-  blue         = '#076678',
-  yellow       = '#af3a03',
-  gray         = '#928374',
-  darkgray     = '#ebdbb2',
-  lightgray    = '#d5c4a1',
-  inactivegray = '#a89984'
+  bg     = '#fbf1c7',
+  fg     = '#3c3836',
+  red    = '#9d0006',
+  green  = '#79740e',
+  blue   = '#076678',
+  orange = '#af3a03',
+  gray   = '#928374',
+  bg1    = '#ebdbb2',
+  bg2    = '#d5c4a1',
+  bg4    = '#a89984'
 }
 
 gruvbox_light.normal = {
-  a = {bg = colors.gray, fg = colors.white, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.black},
-  c = {bg = colors.darkgray, fg = colors.gray}
+  a = {bg = colors.gray, fg = colors.bg, gui = 'bold'},
+  b = {bg = colors.bg2, fg = colors.fg},
+  c = {bg = colors.bg1, fg = colors.gray}
 }
 
 gruvbox_light.insert = {
-  a = {bg = colors.blue, fg = colors.white, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.black},
-  c = {bg = colors.lightgray, fg = colors.black}
+  a = {bg = colors.blue, fg = colors.bg, gui = 'bold'},
+  b = {bg = colors.bg2, fg = colors.fg},
+  c = {bg = colors.bg2, fg = colors.fg}
 }
 
 gruvbox_light.visual = {
-  a = {bg = colors.yellow, fg = colors.white, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.black},
-  c = {bg = colors.inactivegray, fg = colors.white}
+  a = {bg = colors.orange, fg = colors.bg, gui = 'bold'},
+  b = {bg = colors.bg2, fg = colors.fg},
+  c = {bg = colors.bg4, fg = colors.bg}
 }
 
 gruvbox_light.replace = {
-  a = {bg = colors.red, fg = colors.white, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.black},
-  c = {bg = colors.white, fg = colors.black}
+  a = {bg = colors.red, fg = colors.bg, gui = 'bold'},
+  b = {bg = colors.bg2, fg = colors.fg},
+  c = {bg = colors.bg, fg = colors.fg}
 }
 
 gruvbox_light.command = {
-  a = {bg = colors.green, fg = colors.white, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.black},
-  c = {bg = colors.inactivegray, fg = colors.white}
+  a = {bg = colors.green, fg = colors.bg, gui = 'bold'},
+  b = {bg = colors.bg2, fg = colors.fg},
+  c = {bg = colors.bg4, fg = colors.bg}
 }
 
 gruvbox_light.inactive = {
-  a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
-  b = {bg = colors.darkgray, fg = colors.gray},
-  c = {bg = colors.darkgray, fg = colors.gray}
+  a = {bg = colors.bg1, fg = colors.gray, gui = 'bold'},
+  b = {bg = colors.bg1, fg = colors.gray},
+  c = {bg = colors.bg1, fg = colors.gray}
 }
 
 require('lualine').setup{
