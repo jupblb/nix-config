50ae94b06068d1ef900ea5df4fc05ff130c42a7c
jupblb <jupblb@google.com>
2021-04-16 15:50:27 +0200

    Fix lf glow preview

--- common/overlay/lf/previewer.nix
+++ common/overlay/lf/previewer.nix
@@ -3,7 +3,7 @@
 writeShellScript "lf-preview" ''
   case "$1" in
     *.json)       ${jq}/bin/jq --color-output . "$1";;
-    *.md)         ${glow}/bin/glow -s light - "$1";;
+    *.md)         ${glow}/bin/glow -s light -- "$1";;
     *.pdf)        ${poppler_utils}/bin/pdftotext "$1" -;;
     *.tar*|*.zip) ${atool}/bin/atool --list -- "$1";;
     *)            ${bat}/bin/bat --style=numbers --color always "$1";;
