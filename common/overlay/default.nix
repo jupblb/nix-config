60d22389bd88d3d67bd43f26f429394694559ef6
jupblb <jupblb@google.com>
2021-05-06 16:36:32 +0200

    Use "with super" in overlay definition

--- common/overlay/default.nix
+++ common/overlay/default.nix
@@ -1,20 +1,20 @@
-self: super: rec {
-  chromium-wayland = super.callPackage ./chromium-wayland/default.nix {};
+self: super: with super; {
+  chromium-wayland = callPackage ./chromium-wayland/default.nix {};
   fishPlugins      = import ./fish { inherit (super) callPackage; };
-  k8s-test-infra   = super.callPackage ./kubernetes/test-infra.nix {};
-  lf               = super.callPackage ./lf { lf = super.lf; };
-  lf-previewer     = super.callPackage ./lf/previewer.nix {};
-  htop             = super.callPackage ./htop { htop = super.htop; };
-  neovim-nightly   = super.callPackage ./neovim {};
-  pragmata-pro     = super.callPackage ./pragmata-pro {};
-  vimPlugins       = super.vimPlugins // {
-    compe-tabnine = super.vimPlugins.compe-tabnine.overrideAttrs(old: {
+  k8s-test-infra   = callPackage ./kubernetes/test-infra.nix {};
+  lf               = callPackage ./lf { lf = super.lf; };
+  lf-previewer     = callPackage ./lf/previewer.nix {};
+  htop             = callPackage ./htop { htop = super.htop; };
+  neovim-nightly   = callPackage ./neovim {};
+  pragmata-pro     = callPackage ./pragmata-pro {};
+  vimPlugins       = vimPlugins // {
+    compe-tabnine = vimPlugins.compe-tabnine.overrideAttrs(old: {
       postFixup = old.postFixup + ''
-        ln -s ${super.tabnine}/bin/TabNine-deep-* $target/binaries/
+        ln -s ${tabnine}/bin/TabNine-deep-* $target/binaries/
       '';
     });
-    lf-vim        = super.callPackage ./neovim/lf-vim.nix {
-      inherit (super.vimPlugins) lf-vim vim-bbye;
+    lf-vim        = callPackage ./neovim/lf-vim.nix {
+      inherit (vimPlugins) lf-vim vim-bbye;
     };
   };
 }
