fcf5c84fd1648959a111beed5a724190c715042c
jupblb <jupblb@google.com>
2021-08-11 19:03:55 +0200

    Remove kitty remote control

--- common/home.nix
+++ common/home.nix
@@ -120,14 +120,12 @@
         "ctrl+l"       = "neighboring_window right";
       };
       settings    = {
-        allow_remote_control          = "socket-only";
         background                    = "#f9f5d7";
         clipboard_control             =
           "write-clipboard write-primary no-append";
         enabled_layouts               = "splits";
         enable_audio_bell             = "no";
         foreground                    = "#282828";
-        listen_on                     = "unix:/tmp/kitty";
         scrollback_pager              =
           "nvim -c 'setl ft=man | call clearmatches() | autocmd VimEnter * norm G{}'";
         scrollback_pager_history_size = 4096;
