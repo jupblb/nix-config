b684afa352da5122050cf4c4bccf7145a7c6928c
jupblb <jupblb@google.com>
2021-10-10 23:31:06 +0200

    Bump zk

--- common/overlay/zk.nix
+++ common/overlay/zk.nix
@@ -3,18 +3,14 @@
 buildGoModule rec {
   buildInputs  = [ icu ];
   doCheck      = false;
-  patches      = [ (fetchurl {
-    url    = "https://github.com/mickael-menu/zk/commit/162bad446902332bb0f39c8d3911c3bc976a5d21.diff";
-    sha256 = "1j3bnpw66w8kxi75zd399yvlz1hdcrj2faqxjy4kd3dkr340v2yg";
-  }) ];
   pname        = "zk";
   preBuild     = ''buildFlagsArray+=("-tags" "fts5 icu")'';
   src          = fetchFromGitHub {
     owner  = "mickael-menu";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "01fxijs8faf06y58m3n23m18rnbmividirz21wablj56avdl4ibj";
+    sha256 = "0q0h0np26sq8ix831izr94il17bp75680plx9dvq27r5zzidazqb";
   };
-  vendorSha256 = "1dwcc20pd21y8w108zlw82jjwjif4yn4xbw4dhwk1jz5nssybzf0";
-  version      = "0.6.0";
+  vendorSha256 = "12bq2hgd982wcl67bvzra19pfagv9rxivl6yn29vxvviqnzhdd4v";
+  version      = "0.7.0";
 }
