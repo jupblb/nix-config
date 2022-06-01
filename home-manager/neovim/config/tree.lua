4f0c90feedec12fa3fe1def384189f97152f8644
jupblb <jupblb@google.com>
2022-06-01 12:13:48 +0200

    Vim updates

--- home-manager/neovim/config/tree.lua
+++ home-manager/neovim/config/tree.lua
@@ -1,7 +1,47 @@
 require('nvim-tree').setup({
+    actions = {
+        expand_all = {
+            max_folder_discovery = 100,
+        },
+    },
     diagnostics = {
         enable = true,
-        icons = {error = "", hint = "", warning = ""}
+        icons = { error = '', hint = '', warning = '' },
+        show_on_dirs = true,
     },
-    view = {width = 42}
+    renderer = {
+        group_empty = true,
+        highlight_git = true,
+        icons = {
+            glyphs = {
+                default = '',
+                folder = {
+                    default = '',
+                    empty = '',
+                    empty_open = '',
+                    open = '',
+                    symlink = '',
+                    symlink_open = '',
+                },
+                git = {
+                    deleted = '',
+                    ignored = '',
+                    renamed = '',
+                    staged = '',
+                    unmerged = '',
+                    unstaged = '',
+                    untracked = '',
+                },
+                symlink = '',
+            },
+            padding = '  ',
+            show = {
+                folder_arrow = false,
+            },
+        },
+    },
+    view = {
+        -- adaptive_size = true,
+        width = 42,
+    },
 })
