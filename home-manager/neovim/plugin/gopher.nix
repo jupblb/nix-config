32421bbfb0190bfbc22ceaf6604bec4c8beea9ca
jupblb <jupblb@google.com>
2022-07-04 20:35:49 +0200

    Update

--- home-manager/neovim/plugin/gopher.nix
+++ home-manager/neovim/plugin/gopher.nix
@@ -8,7 +8,7 @@ vimUtils.buildVimPlugin rec {
     owner  = "olexsmir";
     repo   = "gopher.nvim";
     rev    = version;
-    sha256 = "sha256-aSUCj9H7W8lKrKS18/I0SLIAqOpO8Zten2VPUU3CT5E=";
+    sha256 = "sha256-bq8jTMWmFn+BL3yU6PRHqsvSk4K+96gE+6qgklphymw=";
   };
 }
 
--- home-manager/zk/neovim.nix
+++ home-manager/zk/neovim.nix
@@ -7,6 +7,6 @@ vimUtils.buildVimPlugin rec {
     owner  = "mickael-menu";
     repo   = pname;
     rev    = version;
-    sha256 = "sha256-F4oRpCdYWmuF7muao4xj6DNwXdVFI+aSD2Dy+tYsuS8=";
+    sha256 = "sha256-n9mvHT4g+G3sEGDlIrh1nV2AAw9Bj7jZVag8YRe3NQE=";
   };
 }
