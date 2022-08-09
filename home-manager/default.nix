d2168bac671a7446ee6355584385d65a8602e6ab
jupblb <jupblb@google.com>
2022-08-09 10:33:30 +0200

    Git config update

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -49,6 +49,7 @@
     git = {
       aliases     = {
         amend = "commit --amend --no-edit";
+        fuck  = "reset --hard HEAD";
         line  = "!sh -c 'git log -L$2,+1:\${GIT_PREFIX:-./}$1' -";
         lines = "!sh -c 'git log -L$2,$3:\${GIT_PREFIX:-./}$1' -";
       };
