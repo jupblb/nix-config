b868d1a1c636d16a35cadbd5fa25a4393202ec10
jupblb <jupblb@google.com>
2022-09-04 18:57:18 +0200

    Add Neovim helper function for formatting Markdown (multiple formatters)

--- home-manager/neovim/config/init.lua
+++ home-manager/neovim/config/init.lua
@@ -8,3 +8,37 @@ vim.keymap.set('n', '<Leader>d', function()
     end
     diagnostics_active = not diagnostics_active
 end)
+
+_G.markdown_formatters = {
+    ['Pandoc (with reference-links)'] = {
+        'pandoc', '--columns=80', '--reference-links', '-s', '-f', 'markdown',
+        '-t', 'markdown-simple_tables-raw_attribute', '-'
+    },
+    ['Pandoc (without reference-links)'] = {
+        'pandoc', '--columns=80', '-s', '-f', 'markdown', '-t',
+        'markdown-simple_tables-raw_attribute', '-'
+    },
+    ['Pandoc (gfm)'] = {
+        'pandoc', '--columns=80', '-s', '-f', 'gfm', '-t', 'gfm', '-'
+    },
+}
+
+local markdown_format = function()
+    local available_formatters = {}
+    for formatter, _ in pairs(_G.markdown_formatters) do
+        table.insert(available_formatters, formatter)
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+    local format = function(formatter)
+        local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
+        local cmd = table.concat(_G.markdown_formatters[formatter], ' ')
+        local output = vim.fn.systemlist(cmd, lines)
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, output)
+    end
+
+    vim.ui.select(
+        available_formatters, { prompt = "Select formatter:" }, format)
+end
+
+vim.api.nvim_create_user_command('MarkdownFormat', markdown_format, {})
