94d8241681a7cbd2e3325c934c0f416c916c9a03
jupblb <jupblb@google.com>
2022-10-04 10:43:22 +0200

    Add temporary neovim-remote fix

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,4 +1,16 @@
 self: super: with super; {
-  calibre-web  = callPackage ./calibre-web { inherit (super) calibre-web; };
-  pragmata-pro = callPackage ./pragmata-pro {};
+  calibre-web   = callPackage ./calibre-web { inherit (super) calibre-web; };
+  neovim-remote = super.neovim-remote.overrideAttrs(_: {
+    # https://github.com/NixOS/nixpkgs/pull/194108
+    patches = [
+      (fetchpatch {
+        url = "https://github.com/mhinz/neovim-remote/commit/56d2a4097f4b639a16902390d9bdd8d1350f948c.patch";
+        hash = "sha256-/PjE+9yfHtOUEp3xBaobzRM8Eo2wqOhnF1Es7SIdxvM=";
+      })
+    ];
+    preCheck = ''
+      export HOME="$(mktemp -d)"
+    '';
+  });
+  pragmata-pro  = callPackage ./pragmata-pro {};
 }
