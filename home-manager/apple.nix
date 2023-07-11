0addd8dd82f6d038986fde1dfcc8f4900547d0c6
jupblb <jupblb@google.com>
2023-07-11 16:38:04 +0200

    Show macOS programs in spotlight

--- home-manager/apple.nix
+++ home-manager/apple.nix
@@ -1,4 +1,30 @@
 { lib, ... }: {
+  disabledModules = ["targets/darwin/linkapps.nix"];
+
+  home.activation = {
+    # https://github.com/nix-community/home-manager/issues/1341#issuecomment-1446696577
+    aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        app_folder="Home Manager Apps"
+        app_path="$(echo ~/Applications)/$app_folder"
+        tmp_path="$(mktemp -dt "$app_folder.XXXXXXXXXX")" || exit 1
+
+        for app in \
+          $(find "$newGenPath/home-path/Applications" -type l -exec \
+            readlink -f {} \;)
+        do
+          $DRY_RUN_CMD /usr/bin/osascript \
+            -e "tell app \"Finder\"" \
+            -e "make new alias file at POSIX file \"$tmp_path\" \
+                                    to POSIX file \"$app\"" \
+            -e "set name of result to \"$(basename $app)\"" \
+            -e "end tell"
+        done
+
+        $DRY_RUN_CMD [ -e "$app_path" ] && rm -r "$app_path"
+        $DRY_RUN_CMD mv "$tmp_path" "$app_path"
+    '';
+  };
+
   programs.kitty = { font.size = lib.mkForce 14; };
 
   targets.darwin.defaults = {
