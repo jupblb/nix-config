b21d0b73376112f5cb6728e26e8e471f526f8161
jupblb <jupblb@google.com>
2023-09-25 09:33:39 +0200

    Vim update

--- home-manager/neovim/config/treesitter.lua
+++ home-manager/neovim/config/treesitter.lua
@@ -3,7 +3,10 @@ local parser_install_dir = vim.fn.expand('$XDG_CACHE_HOME/nvim/parsers')
 require('nvim-treesitter.configs').setup({
     context_commentstring = {
         enable = true,
-        config = { dart = '// %s' },
+        config = {
+            cpp = '// %s',
+            dart = '// %s',
+        },
     },
     highlight = { enable = true, disable = { 'yaml' } },
     incremental_selection = {
