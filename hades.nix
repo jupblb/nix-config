3ef9a37e42f69b202389b72291d0a59b0a441857
jupblb <git@kielbowi.cz>
2023-12-09 22:39:08 +0100

    Enable nixified-ai binary cache

--- hades.nix
+++ hades.nix
@@ -86,6 +86,13 @@
     };
   };
 
+  # https://github.com/nixified-ai/flake#enable-binary-cache
+  nix.settings = {
+    trusted-substituters = ["https://ai.cachix.org"];
+    trusted-public-keys  =
+      ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
+  };
+
   powerManagement.cpuFreqGovernor = "ondemand";
 
   programs = {
